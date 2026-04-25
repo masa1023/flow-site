da2d511da34089741b22ac041e063ffee784358a
Masa <masa373.dev@gmail.com>
2026-04-25 17:07:50 +0900

    refactor: remove unused code

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,17 +3,13 @@ import { ServicesSection } from '@/components/sections/services-section'
 import { ExpertiseSection } from '@/components/sections/expertise-section'
 import { TeamSection } from '@/components/sections/team-section'
 import { ContactSection } from '@/components/sections/contact-section'
-// import { StatsSection } from '@/components/sections/stats-section'
-// import { TestimonialsSection } from '@/components/sections/testimonials-section'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <StatsSection /> */}
       <ServicesSection />
       <ExpertiseSection />
-      {/* <TestimonialsSection /> */}
       <TeamSection />
       <ContactSection />
     </>
